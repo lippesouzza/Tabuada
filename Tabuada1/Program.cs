8dd423fae4a886754065d1fb6062583598b70eb1
lippesouzza <84047056+lippesouzza@users.noreply.github.com>
2022-07-18 11:05:56 -0300

    Tabuada

---
+++ Tabuada1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tabuada1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicando;
+            Console.WriteLine("Informe o Multiplicando");
+            multiplicando = int.Parse(Console.ReadLine());
+
+            for (int multiplicador = 0; multiplicador <= 10; multiplicador++ )
+
+            {
+                Console.WriteLine(multiplicando + " x " + multiplicador + " = " + (multiplicando * multiplicador));
+
+         
+
+            }
+
+
+        }
+    }
+}
